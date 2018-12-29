29cf2e3ef06324114dd969ccca0b1ecce9825c11
aileenperalta <aileenp00@berkeley.edu>
2018-12-28 21:02:29 -0800

    Complete author_age

--- week1/ch02/author_age.rb
+++ week1/ch02/author_age.rb
@@ -4,7 +4,7 @@
 # Use 365.25 days/year to both take leap years into account and get a more accurate decimal
 
 # I wasn't sure how to take the rounding down of integers into my code in order
-# to get a better estimate
+# to get a better estimate for the age
 ### Your Code Here ###
 convert_seconds_to_days = 1160000000 * 1/60 * 1/60 * 1/24 * 1/365.25
-puts convert_seconds_to_days.to_i.to_s + ' years is the author\'s age'
+puts 'The author is ' + convert_seconds_to_days.to_i.to_s +  ' years old.'
