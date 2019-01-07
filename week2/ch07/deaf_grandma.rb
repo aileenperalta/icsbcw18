cec7ba8772d6ad2b5685d9d042785488a4f3e271
aileenperalta <aileenp00@berkeley.edu>
2019-01-06 21:45:36 -0800

    Complete deaf_grandma

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,14 @@
 puts 'COME GIVE GRANNY A KISS!'
-
-while () # Fill in the while condition
-  ### Your Code Here ###
+while true
+  input = gets.chomp.to_s
+  if input == input.downcase
+    puts 'HUH?! SPEAK UP, SONNY!'
+  elsif input != 'BYE'
+    year = rand(21) + 1930
+    puts 'NO, NOT SINCE ' + year.to_s
+  end
+  if input == 'BYE'
+    break
+  end
 end
-
 puts 'OK SONNY, TALK AGAIN SOON'
