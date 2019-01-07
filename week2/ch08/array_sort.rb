436d9f7ea91ac6c982625f80b48098e8f7ad1e90
aileenperalta <aileenp00@berkeley.edu>
2019-01-06 21:47:28 -0800

    Complete array

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -3,8 +3,11 @@ word_list = []
 puts 'Please enter the desired words (one per line), and press enter when finished:'
 
 # Fetch the words, then output the sorted list
-while ()
-
+while true
+  talk = gets.chomp
+  word_list << talk
+  if talk.empty?
+    break
+  end
 end
-
-puts word_list # Make sure its sorted!
+puts word_list.sort # Make sure its sorted!
