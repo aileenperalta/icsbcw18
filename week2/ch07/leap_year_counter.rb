f832d3d24c413fcd9dab0b90cc316d59a3e3945e
aileenperalta <aileenp00@berkeley.edu>
2019-01-06 21:46:33 -0800

    Complete leap_year

--- week2/ch07/leap_year_counter.rb
+++ week2/ch07/leap_year_counter.rb
@@ -2,8 +2,16 @@
 
 ### Get input from user: ###
 print("Please enter a starting year: ")
-starting = gets.chomp ## FIXME?
+starting = gets.chomp.to_i ## FIXME?
 print("Now enter a ending year: ")
-ending = gets.chomp ## FIXME?
-
+ending = gets.chomp.to_i ## FIXME?
 ### Rest of your code here ###
+year  = 0
+year2 = ending - starting
+  if year2 % 400 || year2 % 4 == 0
+    year3 = year2/4
+  end
+  if starting % 400 || starting % 4 == 0
+    year = year + 1
+  end
+puts year + year3
