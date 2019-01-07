c00352ea3098b5279215f164a896a49afe1e0607
aileenperalta <aileenp00@berkeley.edu>
2019-01-06 21:47:52 -0800

    Complete toc

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,9 +1,12 @@
 # Make sure you read the whole tip section, there's some good stuff there!
 
-line_width = # Set it to a default length you'll use throughout
+line_width = 40# Set it to a default length you'll use throughout
 title = "Table of Contents"
 
 # Depending on your solution, you may use more than one array, but here's one to get you started
-chapters = [] 
-
-### Your Code Here ###
+chapter = 1
+all = [['Getting Started', 'page 1'], ['Numbers', 'page 9'], ['Letter', 'page 13']]
+all.each do |no|
+  puts chapter.to_s + ":" + (no[0].ljust(line_width)) + no[1]
+  chapter +=1
+end
