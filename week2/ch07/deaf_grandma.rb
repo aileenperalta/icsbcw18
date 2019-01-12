210fc182b11de43e6dd07fe2ef9f67b1804ddf49
aileenperalta <aileenp00@berkeley.edu>
2019-01-11 22:55:03 -0800

    2Redo grandma

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,11 +1,11 @@
 puts 'COME GIVE GRANNY A KISS!'
 while true
   input = gets.chomp.to_s
-  if input == input.downcase || input == input.capitalize
-    puts 'HUH?! SPEAK UP, SONNY!'
-  elsif input != 'BYE'
+  if input == input.upcase
     year = rand(21) + 1930
     puts 'NO, NOT SINCE ' + year.to_s
+  else
+    puts 'HUH?! SPEAK UP, SONNY!'
   end
   if input == 'BYE'
     break
