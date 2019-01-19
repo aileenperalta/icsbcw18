b4d8dfc5b95065ff1d9ead0bbea222926c428f5a
aileenperalta <aileenp00@berkeley.edu>
2019-01-19 01:21:08 -0800

    Complete factorial

--- week4/recursion_practice/factorial.rb
+++ week4/recursion_practice/factorial.rb
@@ -2,8 +2,12 @@
 # See if you can figure out how to write the method!
 
 def factorial(n)
-
+  if n == 1 || n == 0
+    return 1
+  else
+    return n * factorial(n - 1)
   # YOUR CODE HERE #
+  end
 
 end
 
